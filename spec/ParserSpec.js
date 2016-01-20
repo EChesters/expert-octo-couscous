08ca289cec91a1f1d5523f9e898139a47b245c6a
Elizabeth <Elizabeth.chesters@gmail.com>
2016-01-20 14:24:09 +0000

    Added tests to retrieve environment from Tags

--- spec/ParserSpec.js
+++ spec/ParserSpec.js
@@ -7,12 +7,33 @@ describe("Parser", function() {
 
   it("should be able to retrieve the first given private IP address", function() {
     parser.parse('{"Reservations":[{"Instances":[{"PrivateIpAddress":"172.31.15.172"}]}]}');
-    expect(parser.privateIp).toEqual("172.31.15.172"); 
+    expect(parser.objeto.privateIp).toEqual("172.31.15.172"); 
   });
 
-    it("should be able to retrieve the second given private IP address", function() {
-    parser.parse('{"Reservations":[{"Instances":[{"PrivateIpAddress":"172.31.30.100"}]}]}');
-    expect(parser.privateIp).toEqual("172.31.30.100"); 
+  it("should be able to retrieve the second given private IP address", function() {
+    parser.parse('{"Reservations":[{"Instances":[{"Bob": "woo", "PrivateIpAddress":"172.31.30.100"}]}]}');
+    expect(parser.objeto.privateIp).toEqual("172.31.30.100");
   });
+
+  it("should throw an error when NOT given private IP address", function() {
+    parser.parse('{"Reservations":[{"Instances":[{ }]}]}');
+    expect(parser.objeto.privateIp).toEqual("Not found"); 
+  });
+
+  it("should be able to retrieve the Tag value where key is 'name'", function() {
+    parser.parse('{"Reservations":[{"Instances":[{"Tags":[{"Value":"PCI247-euw1-DEPLOY","Key":"Name"}]}]}]}');
+    expect(parser.objeto.environment).toEqual("PCI247-euw1-DEPLOY");
+  });
+
+  it("should be able to retrieve the Tag value where key is 'name'", function() {
+    parser.parse('{"Reservations":[{"Instances":[{"Tags":[{"Value":"PUBLICAPI-euw1-DEPLOY","Key":"Name"},{"Value":"cog-euw1-iot-deploy-PUBLICAPI","Key":"aws:autoscaling:groupName"}]}]}]}');
+    expect(parser.objeto.environment).toEqual("PUBLICAPI-euw1-DEPLOY");
+  });
+
+  // it("should be able to retrieve the Tag value where key contains 'name'", function() {
+  //   parser.parse('{"Reservations":[{"Instances":[{"Tags":[{"Value":"JAMIE-euw1-DEPLOY","Key":"Name"}]}]}]}', "Jamie");
+  //   expect(parser.objeto.environment).toEqual("JAMIE-euw1-DEPLOY");
+  // });
 
 });
+
--- src/Parser.js
+++ src/Parser.js
@@ -1,9 +1,18 @@
 function Parser() {
 }
 
-Parser.prototype.parse = function(json) {
+Parser.prototype.parse = function(json, key) {
     var instances = JSON.parse(json)['Reservations'][0].Instances
     for(var i in instances) {
-    	this.privateIp = instances[i].PrivateIpAddress
+    	this.objeto  = new Object()
+    	this.objeto.privateIp = instances[i].PrivateIpAddress
+		
+		if(typeof this.objeto.privateIp === "undefined"){  this.objeto.privateIp   = "Not found" }
+	
+		for(var j in instances[i].Tags) {
+    		 if(instances[i].Tags[j].Key == "Name"){
+    			this.objeto.environment = instances[i].Tags[j].Value
+    		}
+    	}
     }
 };
